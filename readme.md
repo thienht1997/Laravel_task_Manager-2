e75691cc7fbcd98b1f5c22a4c26bfa12d53fb35f
= <=>
2019-05-24 11:25:21 +0700

    .

---
+++ readme.md
@@ -0,0 +1,40 @@
+Ứng dụng Task Management 
+#
+Luyện tập cấu hình database, sử dụng migration, seeding
+#
+Mã nguồn được sử dụng để thực hành tại <a href="https://codegym.vn">CodeGym</a>
+#
+<h1>Các bước khởi chạy</h1>
+<h2>Cài đặt composer</h2>
+composer install
+
+#
+<h2>Cài đặt npm</h2>
+npm install
+
+#
+<h2>Cấu hình database</h2>
+Tạo file .env từ file .env mẫu
+
+cp .env.example .env
+
+Cấu hình biến môi trường trong file .env
+
+DB_DATABASE= tên database
+
+DB_USERNAME= tên đăng nhập
+ 
+DB_PASSWORD= mật khẩu
+
+<h3>Tạo key cho ứng dụng</h3>
+
+php artisan key:generate
+
+<h3>Chạy migration và seeding</h3>
+
+php artisan migrate --seed
+
+#
+Chạy project laravel trên địa chỉ http://localhost:8000 với câu lệnh
+
+php artisan serve
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use Illuminate\Support\Facades\Route;
+Route::get('/','TaskController@index');
+//tao group route tasks
+Route::group(['prefix' => 'tasks'], function () {
+  Route::get('/','TaskController@index')->name('tasks.index');
+  Route::get('/create','TaskController@create')->name('tasks.create');
+  Route::post('/create','TaskController@store')->name('tasks.store');
+  Route::get('/{id}/edit','TaskController@edit')->name('tasks.edit');
+  Route::post('/{id}/edit','TaskController@update')->name('tasks.update');
+  Route::get('/{id}/destroy','TaskController@destroy')->name('tasks.destroy');
+});
